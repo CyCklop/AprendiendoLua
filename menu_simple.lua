c280b917b2b2a277ac172650aa9b1e2c6788d7e7
CyCklop <ignacioandresflores06@gmail.com>
2025-10-13 09:36:35 -0300

    learning.3

---
+++ menu_simple.lua
@@ -0,0 +1,16 @@
+-- menu emulando switch case en lua
+
+while true do
+    print("")
+    local opcion = tonumber(io.read())
+
+    if opcion == 1 then
+        print("")
+    elseif opcion == 2 then
+        print("")
+        break
+    else
+        print("")
+    end
+    print()
+end
