68fd5adacd731c9ae028cbe8c1ed749434629f49
CyCklop <ignacioandresflores06@gmail.com>
2025-10-13 12:42:59 -0300

    learning.5

--- menu_simple.lua
+++ menu_simple.lua
@@ -1,16 +1,16 @@
 -- menu emulando switch case en lua
 
 while true do
-    print("")
+    print("Ingrese una opcion\n1.- Ingresar\n2.- Salir\ningrese una opcion: ")
     local opcion = tonumber(io.read())
 
     if opcion == 1 then
-        print("")
+        print("Has elegido la opcion 1")
     elseif opcion == 2 then
-        print("")
+        print("Saliendo...")
         break
     else
-        print("")
+        print("Error, ingrese una opcion valida")
     end
     print()
 end
