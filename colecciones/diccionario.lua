056df71abdb70c7164986f65b9be3e29b51459ad
CyCklop <ignacioandresflores06@gmail.com>
2025-10-13 12:49:59 -0300

    learning.6

---
+++ colecciones/diccionario.lua
@@ -0,0 +1,11 @@
+--diccionario en lua
+
+local diccionario = {
+    nombre = "John",
+    apellido = "Doe",
+    edad = 25
+}
+
+--acceder a valores
+print(diccionario.nombre)
+print(diccionario["edad"])
